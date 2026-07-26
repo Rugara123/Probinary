947f085a177264112951c860d4c1b881ca67cfce
Rugara123 <144539458+Rugara123@users.noreply.github.com>
2026-07-26 19:22:43 +0300

    Add authentication functionality with Firebase

---
+++ auth.js
@@ -0,0 +1,7 @@
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+console.log("Authentication loaded successfully.");
